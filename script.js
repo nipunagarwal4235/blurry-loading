b8eb38846729a48dcede88c612d2cc6cb9052baf
Nipun Agarwal <nipunagarwal2002@gmail.com>
2021-05-07 00:28:00 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const loadText = document.querySelector('.loading-text');
+const loadText2 = document.querySelector('.load-text');
 const bg = document.querySelector('.bg');
 
 let load = 0;
- 
-let int = setInterval(blurring , 30);
+const string = 'Just Travel';
+let int = setInterval(blurring , 40);
 
 function blurring (){
     load++;
     if(load > 99){
         clearInterval(int)
     }
-    loadText.innerText = `${load}% JuSt TrAvEl`;
+    loadText.innerText = `${load}%`;
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`;
+    loadText2.innerText = `${string}`;
+    loadText2.style.opacity = scale(load, 0, 100, 0, 1);
 }
 
 function scale (number, inMin, inMax, outMin, outMax) {
